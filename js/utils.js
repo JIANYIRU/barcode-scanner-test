655d2e3c8665b440a1c31660648e5428bfc6e8f8
JIANYIRU <eggnergyfun@gmail.com>
2026-08-05 15:13:06 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,25 @@
+/**
+ * 避免網址參數被當成 HTML 執行
+ */
+function escapeHtml(value) {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
 
+/**
+ * 取得合法的正整數數量
+ */
+function getValidQuantity(value) {
+  const quantity =
+    Number.parseInt(value, 10);
+
+  if (!Number.isInteger(quantity) || quantity < 1) {
+    return 1;
+  }
+
+  return quantity;
+}
