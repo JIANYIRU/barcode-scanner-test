dacfa5742a073a493194905bffd9514403a89dbb
JIANYIRU <eggnergyfun@gmail.com>
2026-08-05 15:12:37 +0800

    Update scan.js

--- js/scan.js
+++ js/scan.js
@@ -358,17 +358,6 @@ async function changeCameraZoom(event) {
 }
 
 
-/**
- * 避免網址參數被當成 HTML 執行
- */
-function escapeHtml(value) {
-  return String(value)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
-}
 
 
 /**
@@ -436,19 +425,7 @@ function normalizeQuantity() {
     getValidQuantity(quantityInput.value);
 }
 
-/**
- * 取得合法的正整數數量
- */
-function getValidQuantity(value) {
-  const quantity =
-    Number.parseInt(value, 10);
 
-  if (!Number.isInteger(quantity) || quantity < 1) {
-    return 1;
-  }
-
-  return quantity;
-}
 
 /**
  * 加入商品至抄貨列表
